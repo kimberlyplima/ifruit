5f67245dc97deec212e02c98b662327e7fc9eccb
Yan <yanlucaslulu@gmail.com>
2019-05-18 11:39:37 -0300

    login e cadastro funcionando

---
+++ app/src/main/java/br/com/kimberlyplima/ifruit/CadastroActivity.java
@@ -0,0 +1,76 @@
+package br.com.kimberlyplima.ifruit;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    private Button btnCadastro;
+    private EditText txtSenha;
+    private EditText txtSenha2;
+    private EditText txtEmail;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+
+        txtSenha = (EditText) findViewById(R.id.txt_senha);
+        txtEmail = (EditText) findViewById(R.id.txt_email);
+        txtSenha2 = (EditText) findViewById(R.id.txt_senha2);
+        btnCadastro = (Button) findViewById(R.id.btn_cadastro);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        btnCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textoSenha = txtSenha.getText().toString();
+                String textoEmail = txtEmail.getText().toString();
+                String textoSenha2 = txtSenha2.getText().toString();
+                if(TextUtils.isEmpty(textoSenha) || TextUtils.isEmpty(textoSenha2) || TextUtils.isEmpty(textoEmail)){
+                    Toast.makeText(CadastroActivity.this, "O preenchimento dos campos e obrigatorio" , Toast.LENGTH_LONG).show();
+                }else if(!textoSenha.equals(textoSenha2)){
+                    Toast.makeText(CadastroActivity.this, "As senhas não conferem" , Toast.LENGTH_LONG).show();
+                } else {
+                    cadastrarUsuario(textoEmail, textoSenha);
+                }
+            }
+        });
+    }
+
+    private void cadastrarUsuario(String email , String senha){
+        mAuth.createUserWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(CadastroActivity.this,ContaLogadaActivity.class));
+                            Toast.makeText(CadastroActivity.this, "Usuario cadastrado com sucesso",Toast.LENGTH_LONG).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(CadastroActivity.this, "Falha ao cadastrar usuario.",
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+    }
+}
--- app/src/main/java/br/com/kimberlyplima/ifruit/MainActivity.java
+++ app/src/main/java/br/com/kimberlyplima/ifruit/MainActivity.java
@@ -1,13 +1,92 @@
 package br.com.kimberlyplima.ifruit;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtSenha;
+    private EditText txtEmail;
+    private Button btnLogin;
+    private Button btnCadastro;
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtSenha = (EditText) findViewById(R.id.txt_senha);
+        txtEmail = (EditText) findViewById(R.id.txt_email);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnCadastro = (Button) findViewById(R.id.btn_cadastro);
+
+        auth = FirebaseAuth.getInstance();
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if(firebaseAuth.getCurrentUser() != null){
+                    // Descomentar a linha de baixo para aproveitar a sessao logada do usuario
+                   // startActivity(new Intent(MainActivity.this , ContaLogadaActivity.class));
+                }
+
+            }
+        };
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startLogin();
+            }
+        });
+
+        btnCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CadastroActivity.class));
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    private void startLogin () {
+        String email = txtEmail.getText().toString();
+        String senha = txtSenha.getText().toString();
+
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(senha)){
+            Toast.makeText(MainActivity.this,"O preenchimento dos campos e obrigatorio", Toast.LENGTH_LONG).show();
+        } else {
+
+            auth.signInWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        startActivity(new Intent(MainActivity.this , ContaLogadaActivity.class));
+                    } else {
+                        Toast.makeText(MainActivity.this, "E-mail ou senha incorretos", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
     }
 }
