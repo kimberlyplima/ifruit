fbb275380f568ce4d4e6b7893155b11dc0617edd
Yan <yanlucaslulu@gmail.com>
2019-05-22 19:54:38 -0300

    feito tela inicial recycler view

--- app/src/main/java/br/com/kimberlyplima/ifruit/ContaLogadaActivity.java
+++ app/src/main/java/br/com/kimberlyplima/ifruit/ContaLogadaActivity.java
@@ -1,13 +1,49 @@
 package br.com.kimberlyplima.ifruit;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class ContaLogadaActivity extends AppCompatActivity {
+
+    private static final String TAG = "ContaLogadaActivity";
+
+    private ArrayList<String> imageNames = new ArrayList<>();
+    private ArrayList<String> imageUrls = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conta_logada);
+
+        Log.d(TAG,"entrou inicio ContaLogadaActivity");
+
+        initImageBitmaps();
+    }
+
+    private void initImageBitmaps() {
+        imageUrls.add("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
+        imageNames.add("teste");
+
+        imageUrls.add("https://i.imgur.com/ZcLLrkY.jpg");
+        imageNames.add("teste23123");
+
+
+
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this,imageNames,imageUrls);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
