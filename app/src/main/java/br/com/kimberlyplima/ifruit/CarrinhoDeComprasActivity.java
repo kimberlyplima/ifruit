dad17a61b24951b77713c8cfcbfcab53e064737a
Yan <yanlucaslulu@gmail.com>
2019-05-27 18:39:25 -0300

    perfil activity e carrinho activity

---
+++ app/src/main/java/br/com/kimberlyplima/ifruit/CarrinhoDeComprasActivity.java
@@ -0,0 +1,13 @@
+package br.com.kimberlyplima.ifruit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CarrinhoDeComprasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carrinho_de_compras);
+    }
+}
