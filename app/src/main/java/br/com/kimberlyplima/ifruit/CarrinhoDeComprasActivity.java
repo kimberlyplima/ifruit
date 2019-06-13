2947a6e8206f0b339241ac6e55652baf101016ae
Yan <yanlucaslulu@gmail.com>
2019-06-13 07:47:26 -0300

    ajuste crash banco de dados

--- app/src/main/java/br/com/kimberlyplima/ifruit/CarrinhoDeComprasActivity.java
+++ app/src/main/java/br/com/kimberlyplima/ifruit/CarrinhoDeComprasActivity.java
@@ -64,7 +64,7 @@ public class CarrinhoDeComprasActivity extends AppCompatActivity {
         initRecyclerView();
 
 
-        referenciaBD = FirebaseDatabase.getInstance().getReference("carrinhos");
+        //referenciaBD = FirebaseDatabase.getInstance().getReference("carrinhos");
     }
 
     public void aumentarQuantidadeProduto(Produto p){
